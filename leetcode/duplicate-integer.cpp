5c4e7c44dbae1f99b9235ae304d50c163773fbf1
ayushrajdahal <54719576+ayushrajdahal@users.noreply.github.com>
2024-07-15 03:35:07 -0500

    Add Duplicate Integer problem statement: C++

--- leetcode/duplicate-integer.cpp
+++ leetcode/duplicate-integer.cpp
@@ -1,9 +1,28 @@
-class Solution {
+// Duplicate Integer
+
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
+// Example 2:
+
+// Input: nums = [1, 2, 3, 4]
+
+// Output: false
+
+class Solution
+{
 public:
-    bool hasDuplicate(vector<int>& nums) {
+    bool hasDuplicate(vector<int> &nums)
+    {
         unordered_set<int> seen;
-        for (int i = 0; i < nums.size(); i++) {
-            if (seen.find(nums[i]) != seen.end()) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (seen.find(nums[i]) != seen.end())
+            {
                 return true;
             }
             seen.insert(nums[i]);
@@ -11,4 +30,3 @@ public:
         return false;
     }
 };
-

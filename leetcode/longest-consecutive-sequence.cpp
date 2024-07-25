e09dacb98254bfdf9f3f5289a63461a5e1855336
ayushrajdahal <54719576+ayushrajdahal@users.noreply.github.com>
2024-07-24 23:16:37 -0500

    LeetCode: Longest Consecutive Sequence (C++)

---
+++ leetcode/longest-consecutive-sequence.cpp
@@ -0,0 +1,49 @@
+// Longest Consecutive Sequence
+
+// Given an array of integers nums, return the length of the longest consecutive sequence of elements.
+
+// A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element.
+
+// You must write an algorithm that runs in O(n) time.
+
+// Example 1:
+
+// Input: nums = [2,20,4,10,3,4,5]
+
+// Output: 4
+// Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+// Example 2:
+
+// Input: nums = [0,3,2,5,4,6,1,1]
+
+// Output: 7
+// Constraints:
+
+// 0 <= nums.length <= 1000
+// -10^9 <= nums[i] <= 10^9
+
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int length;
+        int max_len = 0;
+        for (int num : nums)
+        {
+
+            if (numSet.find(num) != numSet.end() && numSet.find(num - 1) == numSet.end())
+            {
+                length = 1;
+                while (numSet.find(num + length) != numSet.end())
+                {
+                    length++;
+                }
+                max_len = max(max_len, length);
+            }
+        }
+        return max_len;
+    }
+};

57da3da98c7fd40caeb376596b6a7a3feb4eacc7
ayushrajdahal <54719576+ayushrajdahal@users.noreply.github.com>
2024-08-03 20:32:34 -0500

    LeetCode: Better Solution for House Robber - reduce space complexity to O(1) (C++)

--- leetcode/house-robber.cpp
+++ leetcode/house-robber.cpp
@@ -30,28 +30,20 @@ class Solution
 public:
     int rob(vector<int> &nums)
     {
-        int numsLen = nums.size();
+        // these two variables store maximum possible money that can be robbed from the last two houses
+        int maxRob1 = 0, maxRob2 = 0;
 
-        // base cases: in case length of the array passed is either 0 or 1
-        if (numsLen == 0)
-            return 0;
-        if (numsLen == 1)
-            return nums[0];
+        // interate through the whole list, calculate the max possible robbery for each index and
+        // only store the last two since that's all we'll need to calculate the next max
 
-        // vector to store result for previously seen maxes
-        vector<int> maxRobbery(numsLen, 0);
-
-        // set the first two values
-        maxRobbery[0] = nums[0];
-        maxRobbery[1] = max(nums[0], nums[1]);
-
-        // iterate through the rest of the values, assign it as the max of last seen value and sum of step-over and current value
-        for (int i = 2; i < numsLen; i++)
+        for (int n : nums)
         {
-            maxRobbery[i] = max(maxRobbery[i - 1], maxRobbery[i - 2] + nums[i]);
+            temp = max(maxRob1 + n, maxRob2);
+            maxRob1 = maxRob2;
+            maxRob2 = temp;
         }
 
-        // return the last element of vector
-        return maxRobbery[numsLen - 1];
+        // this stores the max robbery value for the last element in the list
+        return maxRob2
     }
 };
